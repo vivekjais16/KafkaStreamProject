3e81606d74b01c15195778558ffafb3d7f900e69
VIVEK JAISWAL <vivekjaiswal@VIVEKs-MacBook-Air.local>
2025-11-19 01:49:53 +0530

    Initial project upload

---
+++ tracker.py
@@ -0,0 +1,33 @@
+import json
+
+from confluent_kafka import Consumer
+
+consumer_config = {
+    "bootstrap.servers": "localhost:9092",
+    "group.id": "order-tracker",
+    "auto.offset.reset": "earliest"
+}
+
+consumer = Consumer(consumer_config)
+
+consumer.subscribe(["orders"])
+
+print("🟢 Consumer is running and subscribed to orders topic")
+
+try:
+    while True:
+        msg = consumer.poll(1.0)
+        if msg is None:
+            continue
+        if msg.error():
+            print("❌ Error:", msg.error())
+            continue
+
+        value = msg.value().decode("utf-8")
+        order = json.loads(value)
+        print(f"📦 Received order: {order['quantity']} x {order['item']} from {order['user']}")
+except KeyboardInterrupt:
+    print("\n🔴 Stopping consumer")
+
+finally:
+    consumer.close()
